1d96461844663a6641fe99f36f34c1767a348c60
fbsanchez <fborja.sanchez@artica.es>
2016-02-02 12:20:52 +0100

    Fixed password changer

--- coddns_console/var/www/html/coddns/usr/newpass.php
+++ coddns_console/var/www/html/coddns/usr/newpass.php
@@ -1,7 +1,9 @@
 <?php
 
-if (!isset($_SESSION)){
-    session_start();
+if (   (isset ($_SESSION["email"]))
+    || (! isset ($_GET["token"]))) {
+    header ("Location: /");
+    exit (1);
 }
 
 if (!isset ($_SESSION["email"])) {
