9065231191499ccd7d469ed0f4c747097cbf22a2
Fco de Borja Sánchez <fborja.sanchez@artica.es>
2016-02-11 12:48:22 +0100

    Structure def.

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
 <?php
-/* 
- * pub/index.php
- * Folder: pub
- *  Switches the received ID as manual page to be displayed
+/**
+ * <copyright company="CODDNS">
+ * Copyright (c) 2013 All Right Reserved, http://coddns.es/
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, NO INCLUDING THE WARRANTIES OF
+ * MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * </copyright>
+ * <author>Fco de Borja Sanchez</author>
+ * <email>fborja.sanchezs@gmail.com</email>
+ * <date>2016-02-11</date>
+ * <update>2016-02-11</udate>
+ * <summary> </summary>
  */
+
 
 include_once (dirname(__FILE__) . "/../include/config.php");
 require_once (dirname(__FILE__) . "/../lib/ipv4.php");
 require_once (dirname(__FILE__) . "/../lib/util.php");
+require_once (dirname(__FILE__) . "/../lib/coduser.php");
+
+if (! defined("_VALID_ACCESS")) { // Avoid direct access
+    header ("Location: " . $config["html_root"] . "/");
+    exit (1);
+}
+
+$auth_level_required = get_required_auth_level('cms','','');
+$user = new CODUser();
+$user->check_auth_level($auth_level_required);
 
 ?>
 
@@ -15,12 +36,14 @@ require_once (dirname(__FILE__) . "/../lib/util.php");
 <html>
 
 <body>
-        <h1>Manuales</h1>
+	<section>
+        <h2>Manuales</h2>
 
         <p>Navegue por el &iacute;ndice y elija el manual que desee</p>
 
         <div id="pub_index">
         </div>
+    </section>
 
 </body>
 
