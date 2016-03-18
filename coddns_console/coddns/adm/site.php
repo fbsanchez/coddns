fec16778958c9c77907779aefdd9500d5135eb2a
danielbarmar85 <daniel.barbero@artica.es>
2016-03-18 20:19:34 +0100

    continuing with site.php development

--- coddns_console/coddns/adm/site.php
+++ coddns_console/coddns/adm/site.php
@@ -39,23 +39,57 @@ $user->check_auth_level($auth_level_required);
 	<link rel="stylesheet" type="text/css" href="rs/css/pc/adm_site.css">
 </head>
 <script languague="javascript">
-	 
+	 function linkselected(id_elem){
+	 	var id = document.getElementById(id_elem);
+	 	
+	 	//diferents id links
+	 	var link_users  = document.getElementById('link_users');
+	 	var link_roles  = document.getElementById('link_roles');
+	 	var link_groups = document.getElementById('link_groups');
+	 	var link_acls   = document.getElementById('link_acls');
+
+	 	//remove class selected the all links
+	 	link_users.className  = link_users.className.replace('selected', '');
+	 	link_roles.className  = link_roles.className.replace('selected', '');
+	 	link_groups.className = link_groups.className.replace('selected', '');
+	 	link_acls.className   = link_acls.className.replace('selected', '');
+
+	 	//revert img original
+	 	link_users.childNodes[1].childNodes[1].setAttribute("src", "<?php echo $config["html_root"] . "/rs/img/teamwork-in-the-office.png"; ?>");
+	 	link_roles.childNodes[1].childNodes[1].setAttribute("src", "<?php echo $config["html_root"] . "/rs/img/group-of-businessmen.png"; ?>");
+	 	link_groups.childNodes[1].childNodes[1].setAttribute("src", "<?php echo $config["html_root"] . "/rs/img/web-5.png"; ?>");
+	 	link_acls.childNodes[1].childNodes[1].setAttribute("src", "<?php echo $config["html_root"] . "/rs/img/key-to-success.png"; ?>");
+
+	 	//add class selected on click link
+	 	id.className = 'selected';
+
+	 	//change img onclick link
+	 	if (id_elem == 'link_users'){
+	 		id.childNodes[1].childNodes[1].setAttribute("src", "<?php echo $config["html_root"] . "/rs/img/business.png"; ?>");
+	 	} else if (id_elem == 'link_roles'){
+	 		id.childNodes[1].childNodes[1].setAttribute("src", "<?php echo $config["html_root"] . "/rs/img/people-1.png"; ?>");
+	 	} else if (id_elem == 'link_groups'){
+	 		id.childNodes[1].childNodes[1].setAttribute("src", "<?php echo $config["html_root"] . "/rs/img/web-white-5.png"; ?>");
+	 	} else {
+	 		id.childNodes[1].childNodes[1].setAttribute("src", "<?php echo $config["html_root"] . "/rs/img/people-2.png"; ?>");
+	 	}
+	 }
 </script>
 <body>
 	<section>
 		<h2>Panel de administraci&oacute;n del sitio</h2>
-		<nav>
-			<ul id="tabs">
-				<li><a href="#users" onclick="javascript:updateContent('adm_site_section', 'ajax.php', 'action=list_users');" title="Usuarios">
+		<nav
+>			<ul id="tabs">
+				<li><a href="javascript:updateContent('adm_site_section', 'ajax.php', 'action=list_users');" onclick="linkselected('link_users')" title="Usuarios" id="link_users">
 				<div class="menu_button">
 				<img src="<?php echo $config["html_root"] . "/rs/img/teamwork-in-the-office.png"; ?>" alt="Service Settings"/><p>Usuarios</p></div></a></li>
-				<li><a href="#groups" onclick="javascript:updateContent('adm_site_section', 'ajax.php', 'action=list_groups');" title="Grupos">
+				<li><a href="javascript:updateContent('adm_site_section', 'ajax.php', 'action=list_groups');" onclick="linkselected('link_groups')" title="Grupos" id="link_groups">
 				<div class="menu_button">
 				<img src="<?php echo $config["html_root"] . "/rs/img/web-5.png"; ?>" alt="Service Settings"/><p>Grupos</p></div></a></li>
-				<li><a href="#roles" onclick="javascript:updateContent('adm_site_section', 'ajax.php', 'action=list_roles');" title="Roles">
+				<li><a href="javascript:updateContent('adm_site_section', 'ajax.php', 'action=list_roles');" onclick="linkselected('link_roles')" title="Roles" id="link_roles">
 				<div class="menu_button">
 				<img src="<?php echo $config["html_root"] . "/rs/img/group-of-businessmen.png"; ?>" alt="Roles"/><p>Roles</p></div></a></li>
-				<li><a href="#acls" onclick="javascript:updateContent('adm_site_section', 'ajax.php', 'action=list_acls');" title="ACLs">
+				<li><a href="javascript:updateContent('adm_site_section', 'ajax.php', 'action=list_acls');" onclick="linkselected('link_acls')" title="ACLs" id="link_acls">
 				<div class="menu_button">
 				<img src="<?php echo $config["html_root"] . "/rs/img/key-to-success.png"; ?>" alt="Service Settings"/><p>Acls</p></div></a></li>
 			</ul>
@@ -66,10 +100,8 @@ $user->check_auth_level($auth_level_required);
 
 		</div>
 	</section>
-	<div class="button_return">	
-		<a href="<?php echo $config["html_root"] . "/?m=adm" ?>" title="Return">
-		<img src="<?php echo $config["html_root"] . "/rs/img/web-7.png"; ?>" alt="Service Settings"/></a>
-	</div>
+	<a href="<?php echo $config["html_root"] . "/?m=adm" ?>" title="Return" class="button_return">
+	<img src="<?php echo $config["html_root"] . "/rs/img/web-7.png"; ?>" alt="Service Settings"/></a>
 </body>
 
 </html>
