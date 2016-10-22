26a64eaeb59bc472fe9e1f4859d360920cf9530a
daniel barbero <root@localhost.localdomain>
2016-10-22 02:17:08 +0200

    test

--- coddns_console/coddns/adm/adm_rq_newgroup.php
+++ coddns_console/coddns/adm/adm_rq_newgroup.php
@@ -42,8 +42,9 @@ if(!$error){
 	$q = "insert into groups (tag, description) values ('" . $_POST["tag"] . "', '" . $_POST["description"] . "');";
 	$dbclient->exeq($q) or die($dbclient->lq_error());
 	$dbclient->disconnect();
+	//<script type="text/javascript">location.reload();</script>
 ?>
-	<script type="text/javascript">location.reload();</script>
+	
 <?php
 }
 ?>
